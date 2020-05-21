ffe1e271161a2bbdca58ceebad4fec97b8c6b5d4
Rashmi <poddar.rashmi@gmail.com>
2020-05-20 23:56:17 -0400

    Implemented climbing stairs using recursion

--- DynamicProgramming/climbingStairs.js
+++ DynamicProgramming/climbingStairs.js
@@ -3,16 +3,35 @@
 // In how many distinct ways can you climb to the top
 
 // Approach 1 DP - Bottom to top 
-const climbingStairs = (n) => {
-  let steps = [0, 1, 2];    
-  let count = 3;
-  while (count <= n) {
-    steps.push(steps[count-1] + steps[count-2]);
-    count += 1
-  }
+// const climbingStairs = (n) => {
+//   let steps = [0, 1, 2];    
+//   let count = 3;
+//   while (count <= n) {
+//     steps.push(steps[count-1] + steps[count-2]);
+//     count += 1
+//   }
     
-  return steps[n];
-}
+//   return steps[n];
+// }
+
+// Approach 2 - Recursion with memoization
+memo = {};
+const climbingStairs = (n) => {
+  if (memo[n]) {
+    return memo[n];
+  }
+  if (n < 2) {
+    return n;
+  }
+  
+  if (n === 2) {
+    return 2;       
+  }
+  
+  memo[n] = climbingStairs(n-1) + climbingStairs(n-2);
+  return memo[n];
+};
+
 
 
 console.log(climbingStairs(2)); // 2
