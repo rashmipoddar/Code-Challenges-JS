d4275e58f7937854620303904d887e1ff950fcc4
Rashmi <poddar.rashmi@gmail.com>
2020-06-03 23:57:05 -0400

    Implemented next greater letter using binary search

--- Strings/nextGreaterLetter.js
+++ Strings/nextGreaterLetter.js
@@ -2,13 +2,28 @@
 // Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
 
 // Approach 1
+// const nextGreaterLetter = (letters, target) => {
+//   for (let i = 0; i < letters.length; i++) {
+//     if (letters[i] > target) {
+//       return letters[i];
+//     }
+//   }
+//   return letters[0];  
+// }
+
+// Approach 2: Binary Search
 const nextGreaterLetter = (letters, target) => {
-  for (let i = 0; i < letters.length; i++) {
-    if (letters[i] > target) {
-      return letters[i];
+  let start = 0;
+  let end = letters.length;
+  while (start < end) {
+    let mid = Math.floor((start+end) / 2);
+    if (letters[mid] <= target) {
+      start = mid + 1;
+    } else {
+      end = mid;
     }
   }
-  return letters[0];  
+  return letters[start % letters.length];
 }
 
 console.log(nextGreaterLetter(["c", "f", "j"], "a")) // c
@@ -18,4 +33,3 @@ console.log(nextGreaterLetter(["c", "f", "j"], "g")); // j
 console.log(nextGreaterLetter(["c", "f", "j"], "j")); // c
 console.log(nextGreaterLetter(["c", "f", "j"], "k")); // c
 console.log(nextGreaterLetter(["e","e","e","e","e","e","n","n","n","n"], "e")); // n
-console.log(nextGreaterLetter());
